4baa17afe62ba625cd16215dec824f3b92d1ee88
LK <lionking71685@gmail.com>
2019-10-18 17:57:39 -0700

    first push

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
+const logger = require('winston');
+const bootstrap = require('./config/bootstrap');
+const config = require('./config');
+const buildApp = require('./src/app');
+
+function startApp() {
+    logger.info('Starting applicationsdfsdfsdfwe3rwrwerwerwerwe234234324');
+    const app = buildApp();
+
+    // Define server options
+    const options = {
+        key: fs.readFileSync('./config/ssl/private.key'),
+        cert: fs.readFileSync('./config/ssl/primary.crt')
+    };
+
+    // Create the https server
+    https.createServer(options, app).listen(config.https.port, () => {
+        logger.info('Https server is now running on port', config.https.port);
+    });
+
+    // Create the http server
+    http.createServer(app).listen(config.http.port, () => {
+        logger.info('Http server is now running on port', config.http.port);
+    });
+}
+
+async function start() {
+    try {
+        await bootstrap();
+        startApp();
+    } catch (e) {
+        logger.error('An error occured when attempting to start application', e);
+    }
+}
+
+start();
